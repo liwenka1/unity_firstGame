f36df4486bc8f4f1657104bfddf632fb04c385a7
一鵺靊一 <2020583117@qq.com>
2022-09-26 17:51:05 +0800

    第一次提交

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+
+    public float speed, jumpForce;
+    private float horizontalMove;
+    public Transform groundCheck;
+    public LayerMask ground;
+    public int Cherry;
+
+    public Text CherryNum;
+
+    public bool isGround, isJump, isCrouch;
+
+    bool jumpPressed;
+    int jumpcount;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpcount > 0)
+        {
+            jumpPressed = true;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            isCrouch = true;
+        }
+        else
+        {
+            isCrouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
+
+        GroundMovement();
+
+        Jump();
+
+        Crouch();
+
+        SwitchAnim();
+    }
+
+    void GroundMovement()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");//Ö»·µ»Ø-1£¬0£¬1
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+
+        if (horizontalMove != 0)
+        {
+            transform.localScale = new Vector3(horizontalMove, 1, 1);
+        }
+
+    }
+
+    void Jump()
+    {
+        if (isGround)
+        {
+            jumpcount = 2;
+            isJump = false;
+        }
+        if (jumpPressed && isGround)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpcount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpcount > 0 && isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpcount--;
+            jumpPressed = false;
+        }
+    }
+
+    void Crouch()
+    {
+        if (isGround && isCrouch)
+        {
+            coll.enabled = false;
+        }
+        else
+        {
+            coll.enabled = true;
+        }
+    }
+
+    void SwitchAnim()
+    {
+        anim.SetFloat("running", Mathf.Abs(rb.velocity.x));
+
+        if (isGround)
+        {
+            anim.SetBool("falling", false);
+        }
+        else if (!isGround && rb.velocity.y > 0)
+        {
+            anim.SetBool("jumping", true);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            anim.SetBool("jumping", false);
+            anim.SetBool("falling", true);
+        }
+
+        if (isCrouch)
+        {
+            anim.SetFloat("running", 0);
+            anim.SetBool("crouching", true);
+        }
+        else
+        {
+            anim.SetBool("crouching", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collection" && collision.isActiveAndEnabled)
+        {
+            Destroy(collision.gameObject);
+            Cherry += 1;
+            CherryNum.text = Cherry.ToString();
+        }
+    }
+}
